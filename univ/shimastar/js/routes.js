09b3f445e77c55adc658b616ea233f0f03374c1d
Sunny-JP <kajimalu10@gmail.com>
2024-09-12 19:20:41 +0900

    first commit

---
+++ univ/shimastar/js/routes.js
@@ -0,0 +1,12 @@
+
+var routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
